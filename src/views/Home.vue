e6c16f2023d8c3aa6ad97a5709f0428a72c4127e
BuildTools <unconfigured@null.spigotmc.org>
2025-10-19 19:40:55 +0300

    fix: Ana sayfa tam ekran kaplaması için min-height: 100vh eklendi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -80,14 +80,14 @@ html, body {
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
 }
 
 .main-content {
   display: flex;
   flex: 1;
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
 }
 
 .left-side {
