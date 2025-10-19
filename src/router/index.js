bbddf9ef6aab9272372741649b14c94ac8feac98
BuildTools <unconfigured@null.spigotmc.org>
2025-10-19 19:12:28 +0300

    fix: Vue Router base ayarı GitHub Pages için düzeltildi

--- src/router/index.js
+++ src/router/index.js
@@ -48,7 +48,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/makportfoliodemo/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return { top: 0 } // Her sayfada yukarıdan başla
